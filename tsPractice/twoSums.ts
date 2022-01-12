48f7657d0122afb16e9774166f9eb6821154ff06
sakethgetla <saketh.getla@protonmail.com>
2022-01-12 23:16:13 +1100

    done 1 today

--- tsPractice/twoSums.ts
+++ tsPractice/twoSums.ts
@@ -1,18 +1,24 @@
-console.log('here');
-var j:any;
-var n:any = "atireasn b c"
+// console.log('here');
+// var j: any;
+// var n: any = "atireasn b c"
 
-for(j in n) {
-   console.log(n[j])
-}
+// for (j in n) {
+//     console.log(n[j])
+// }
 
 
 function twoSum(nums: number[], target: number): number[] {
     let map = new Map();
-    for(var: number numr in nums){
-        if (map.has(target - numr)){
-            return
+    for (var i = 0; i < nums.length; ++i) {
+        if (map.has(target - nums[i])){
+            return [i, map.get(target - nums[i])]
+        } else {
+            map.set(nums[i], i)
         }
 
     }
+    return [0];
 };
+
+
+console.log(twoSum([2, 7, 11, 15], 9));
