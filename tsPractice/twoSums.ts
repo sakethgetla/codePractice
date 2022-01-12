f52f3dea37e3adcf050c2038a5ad816d88a40441
sakethgetla <saketh.getla@protonmail.com>
2022-01-12 22:57:03 +1100

    added typescript

---
+++ tsPractice/twoSums.ts
@@ -0,0 +1,18 @@
+console.log('here');
+var j:any;
+var n:any = "atireasn b c"
+
+for(j in n) {
+   console.log(n[j])
+}
+
+
+function twoSum(nums: number[], target: number): number[] {
+    let map = new Map();
+    for(var: number numr in nums){
+        if (map.has(target - numr)){
+            return
+        }
+
+    }
+};
