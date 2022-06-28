6ebb80400878a262578eb0eaaab66f4a2de395db
sakethgetla <saketh.getla@protonmail.com>
2022-06-28 21:05:34 +1000

    failed to do one today.

---
+++ cpp/minDeletions.cpp
@@ -0,0 +1,25 @@
+#include <cstdlib>
+#include <string>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int minDeletions(string s){
+    // make a 2d frquency array
+    // sort arry based on frequency
+    // go from largest to smallest, if duplicate freq count dist to next smallest by using
+        // vector<int[2]> freq;
+        vector<int[2]> freq;
+    for(char c : s){
+
+    }
+
+    return 0;
+}
+
+int main(){
+    string s = "   000020130 aristeni";
+    cout << minDeletions(s) << endl;
+    return 0;
+}
