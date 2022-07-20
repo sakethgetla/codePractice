380ffc778597dc61ac90a4e9c6179e2dd5e1259e
sakethgetla <saketh.getla@protonmail.com>
2022-07-20 19:26:20 +1000

    done 1 mid. found a better sol and implemented it.

---
+++ cpp/numMatchingSubseq.cpp
@@ -0,0 +1,159 @@
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+
+template <class T>
+void printVector(vector<T> vec){
+    for (T v : vec){
+        cout << v << "->" ;
+    }
+    cout << endl;
+    // cout << "eof" << endl;
+}
+
+
+template <class T>
+void print2DVector(vector<vector<T>> vec){
+    cout << '[' << endl;
+    for (vector<T> v : vec){
+        printVector(v);
+    }
+    cout <<']' << endl;
+    // cout << endl;
+}
+
+// int numMatchingSubseq(string s, vector<string> &words){
+//     int num = 0;
+
+//     int i = 0;
+//     for(string word: words){
+//         i = 0;
+//         for(char c: s){
+//             if(c == word[i])
+//                 i++;
+//             if(i >= word.length()){
+//                 num++;
+//                 break;
+//             }
+//         }
+//     }
+
+//     return num;
+// }
+
+
+// int nextOccur(vector<int> vec, int i){
+//     for(int v: vec){
+//         if (i < v){
+//             return v;
+//         }
+//     }
+//     return -1;
+// }
+
+// int numMatchingSubseq(string s, vector<string> &words){
+//     // unordered_map<char, vector<int>> m;
+//     vector<int> occur[26];
+//     // int i = 0;
+//     int num = 0;
+//     // for (char c: s){
+//     for(int i = 0 ; i < s.length() ; i++){
+//         occur[s[i]-'a'].push_back(i);
+//     }
+
+//     for(string word : words){
+//         int curr = -1;
+//         int match = 0;
+//         // find word
+//         bool found = true;
+//         for(char c: word){
+
+//             found = false;
+//             for(int v : occur[c-'a']){
+//                 if (curr < v){
+//                     curr = v;
+//                     found = true;
+//                     break;
+//                 }
+//             }
+
+//             if (!found){
+//                 break;
+//             }
+//         }
+//         if (found){
+//             num++;
+//         }
+//     }
+
+//     return num;
+
+// }
+
+int numMatchingSubseq(string s, vector<string> &words){
+    vector<char*>  pending[26];
+    int num = 0;
+
+    // for (int i = 0; i < s.length() ; i++){
+    for (int i = 0; i < words.size() ; i++){
+        pending[words[i][0]-'a'].push_back(&words[i][0]);
+    }
+
+
+    // for (vector<char*> c: pending){
+    // for (int i = 0; i < 26 ; i++){
+    //     cout << (char) (i+'a') << ", " ;
+    //     printVector(pending[i]);
+    // }
+
+    for(char c : s){
+        vector<char*> curr = pending[c-'a'];
+        pending[c-'a'] = {};
+
+        for (char * t: curr){
+            // cout << (char) *t << ", " << endl;
+            // cout << t << ", " << endl;
+            // cout << (char) *t - 'a' << ", " << endl;
+            t++;
+            // cout << t << ", " << endl;
+            // cout << t << ", " << (*t == 0) << ", " << endl;
+            if (*t == 0){
+                num++;
+                // cout << "here" << endl;
+            } else {
+                // cout << *t - 'a';
+                // cout << (char) *t - 'a' << ", " << endl;
+                // cout << (char) *t << ", " << endl;
+                pending[*t-'a'].push_back(t);
+            }
+        }
+    }
+
+    // for (int i = 0; i <= s.length(); i++ ){
+    //     cout << (int) s[i] << ',';
+    // }
+    // cout << '<' << endl;
+
+
+
+    return num;
+}
+
+int main(){
+    string s = "abcde";
+    vector<string> words = {"a", "bb", "acd", "ace", "abcde"};
+    // s = "a";
+    cout << s << endl;
+    cout << numMatchingSubseq(s, words) << endl;
+
+    s = "dsahjpjauf";
+    words = {"ahjpjau","ja","ahbwzgqnuk","tnmlanowax"};
+    cout << numMatchingSubseq(s, words) << endl;
+
+    return 0;
+}
--- cpp/test.cpp
+++ cpp/test.cpp
@@ -1,11 +1,43 @@
 #include <cstdlib>
-#include<iostream>
+#include <iostream>
+#include <vector>
 
 using namespace std;
-    const long &a = NULL;
+    // const long &a = NULL;
+
+
+void p(int i){
+ cout << i << endl;
+}
 
 int main(){
-    cout << &a << endl;
-    cout << &a << endl;
+    // int i = 0;
+    // cout << i << endl;
+    // p(i++);
+    // p(i);
+    // // p(++i);
+    // // i++;
+    // cout << i << endl;
+
+
+    string s = "abcd";
+    cout << s << endl;
+    char *c = &s[0];
+
+    vector<char*> a = {&s[0], &s[2]};
+
+
+    for(char* b: a){
+        // cout << (char) *b << endl;
+        cout << *b << endl;
+        b++;
+        cout << *b << endl;
+    }
+
+    // cout << c << endl;
+    // c++;
+    // cout << c << endl;
+
+
     return 0;
 }
