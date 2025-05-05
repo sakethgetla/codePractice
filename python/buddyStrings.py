b97487d56651651d8188bb8b11863cf88f91a0c9
sakethgetla <saketh.getla@protonmail.com>
2025-05-05 13:11:16 +1000

    1 mid, 1 easy

--- python/buddyStrings.py
+++ python/buddyStrings.py
@@ -1,23 +1,38 @@
 from typing import List
 
 class Solution:
-    # dp, very slow
-    def buddyStrings(self, st: str, goal: str) -> bool:
-        if len(st) != len( goal ):
-            return False
+    # def buddyStrings(self, st: str, goal: str) -> bool:
+    #     swap = None
+    #     if len(st) != len( goal ):
+    #         return False
+    #     dic = {}
+    #     for i in range(len(goal)):
+    #
+    #
+    #     for i in range(len(st)):
+    #         if st[i] != goal[i]:
+    #
+    #
 
-        dic = {}
-        for i in range(len(st)):
-            if not st[i] in dic or (st[i] in dic and dic[st[i]] == st[i]) :
-                dic[st[i]] = i
-                
-            if goal[i] in dic:
-                if (st[0:dic[goal[i]]] + st[i] + st[dic[goal[i]]+1:i] + st[dic[goal[i]]] + st[i+1:]) == goal:
-                    return True
-                    
 
-        return False
 
+    # # dp, very slow
+    # def buddyStrings(self, st: str, goal: str) -> bool:
+    #     if len(st) != len( goal ):
+    #         return False
+    #
+    #     dic = {}
+    #     for i in range(len(st)):
+    #         if not st[i] in dic or (st[i] in dic and dic[st[i]] == st[i]) :
+    #             dic[st[i]] = i
+    #             
+    #         if goal[i] in dic:
+    #             if (st[0:dic[goal[i]]] + st[i] + st[dic[goal[i]]+1:i] + st[dic[goal[i]]] + st[i+1:]) == goal:
+    #                 return True
+    #                 
+    #
+    #     return False
+    #
     # # brute force
     # def buddyStrings(self, st: str, goal: str) -> bool:
     #     for i in range(len(st)-1):
@@ -33,29 +48,36 @@ class Solution:
 
 
 
-    # def buddyStrings(self, st: str, goal: str) -> bool:
-    #     if len(st) != len( goal ):
-    #         return False
-    #
-    #     i, j = 0, len(st)-1
-    #     while i < j:
-    #         if (st[i] == goal[j] and st[j] == goal[i]):
-    #             print(i, j)
-    #             if (st[0:i] + st[j] + st[i+1:j] + st[i] + st[j+1:]) == goal:
-    #                 return True
-    #             i += 1
-    #             j -=1
-    #
-    #         elif (st[j] != goal[j] and st[i] != goal[i] ):
-    #             return False
-    #
-    #         if (st[i] == goal[i]):
-    #             i += 1
-    #         if (st[j] == goal[j]):
-    #             j -=1
-    #
-    #     return False
-    #     
+    def buddyStrings(self, st: str, goal: str) -> bool:
+        if len(st) != len( goal ):
+            return False
+
+        i, j = 0, len(st)-1
+        swap = None
+        while i < j:
+            print(i, j, st[i], st[j], goal[i], goal[j], swap)
+            if (st[j] != goal[j] and st[i] != goal[i]) :
+                if (st[i] == goal[j] and st[j] == goal[i]):
+                    if swap == 'must-swap':
+                        return False
+                    else:
+                        swap = 'must-swap'
+                        i += 1
+                        j -=1
+                else:
+                    return False
+            if (st[j] == st[i] and goal[i] == goal[j] and swap != 'must-swap') :
+                swap = 'swapable'
+            
+
+            if (st[i] == goal[i]):
+                i += 1
+            if (st[j] == goal[j]):
+                j -=1
+
+        print(swap)
+        return False if swap == None or (i == j and st[i] != goal[j]) else True
+
 
 
     # def buddyStrings(self, st: str, goal: str) -> bool:
@@ -88,55 +110,61 @@ class Solution:
 a = 'abdc'
 b = 'bacd'
 s = Solution.buddyStrings(Solution(), a, b)
-assert s == False
 print(a,b,s)
+assert s == False
 
 # True
 a = 'abc'
 b = 'bac'
 s = Solution.buddyStrings(Solution(), a, b)
-assert s == True
 print(a,b,s)
+assert s == True
 
 # True
 a = 'ab'
 b = 'ba'
 s = Solution.buddyStrings(Solution(), a, b)
-assert s == True
 print(a,b,s)
+assert s == True
 
 # True
 a = 'aa'
 b = 'aa'
 s = Solution.buddyStrings(Solution(), a, b)
-assert s == True
 print(a,b,s)
+assert s == True
 
 # False
 a = 'ab'
 b = 'ab'
 s = Solution.buddyStrings(Solution(), a, b)
-assert s == False
 print(a,b,s)
+assert s == False
 
 # False
 a = 'aaaaaabc'
 b = 'aaaaaacb'
 s = Solution.buddyStrings(Solution(), a, b)
-assert s == True
 print(a,b,s)
+assert s == True
+
+a = 'abcbe'
+b = 'ababe'
+s = Solution.buddyStrings(Solution(), a, b)
+print(a,b,s)
+assert s == False
 
 a = 'aba'
 b = 'abab'
 s = Solution.buddyStrings(Solution(), a, b)
-assert s == False
 print(a,b,s)
+assert s == False
 
 a = 'abab'
 b = 'abab'
 s = Solution.buddyStrings(Solution(), a, b)
-assert s == True
 print(a,b,s)
+assert s == True
 
 # s = Solution.buddyStrings(Solution(), a, b)
 # assert s == False
--- python/closeStrings.py
+++ python/closeStrings.py
@@ -1,24 +1,88 @@
-
 class Solution:
     def closeStrings(self, word1: str, word2: str) -> bool:
         if len( word2 ) != len(word1):
             return False
+        def createDic(word:str):
+            dic = {}
+            for c in word:
+                if c in dic:
+                    dic[c] += 1
+                else:
+                    dic[c] = 1
+            return dic
+        dic1 = createDic(word1)
+        dic2 = createDic(word2)
 
-        if (word1[0] != word2[0]):
-            # swap
-            for i in range(1, len(word2)):
-                if ( )
+        # s1 = dic1.values().sort()
+        # s1 = [ f for f in dic1.values() ].sort()
+        # s2 = [  f for f in dic2.values() ].sort()
+        # s1 = dic1.values()
+        # s2 = dic2.values()
+        s1 = [f for f in dic1.values() ]
+        s2 = [f for f in dic2.values() ]
+        s1.sort()
+        s2.sort()
+        # s1 = set(dic1.values())
+        # s2 = set(dic2.values())
+
+        print(dic1, dic2)
+        # print(s1, s2)
+        # print(s1.sort(), s2.sort())
+        print(s1, s2)
+        # print(s1.sort(), s2.sort())
+        return s2 == s1 and dic1.keys() == dic2.keys()
+        # return s2 == s1 and set( dic1.keys() ) == set( dic2.keys() )
+        # return True if (s2 == s1) else False
+
+        # for k in dic1.keys():
+        #     if dic1[k] != dic2[k]:
+        #         return False
+        # return True
 
 
 
-        # if:
-        return True if len(word1) == 1 and word1[0] == word2[0] else self.closeStrings(word1[1:], word2[1:])
+
+    # def closeStrings(self, word1: str, word2: str) -> bool:
+    #     if len( word2 ) != len(word1):
+    #         return False
+    #
+    #     if (word1[0] != word2[0]):
+    #         # swap
+    #         for i in range(1, len(word2)):
+    #             if ( )
+    #
+    #
+    #
+    #     # if:
+    #     return True if len(word1) == 1 and word1[0] == word2[0] else self.closeStrings(word1[1:], word2[1:])
 
 
 s = Solution()
 w1 = "abc"
 w2 = "bca"
 o = s.closeStrings(w1, w2)
-print(o)
+print(w1, w2, o)
+assert o == True
 
         
+w1 = "uau"
+w2 = "ssx"
+o = s.closeStrings(w1, w2)
+print(w1, w2, o)
+assert o == False
+
+        
+        
+w1 = "aaabbbbccddeeeeefffff"
+w2 = "aaaaabbcccdddeeeeffff"
+o = s.closeStrings(w1, w2)
+print(w1, w2, o)
+assert o == False
+
+        
+        
+w1 = "cabbba"
+w2 = "abbccc"
+o = s.closeStrings(w1, w2)
+print(w1, w2, o)
+assert o == True
